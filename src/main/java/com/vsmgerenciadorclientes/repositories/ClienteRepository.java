c07afd15a552e73d9a00e8d1fb711b628505ea62
leandrosouzasnts <1eandrosouzasnts@gmail.com>
2024-01-22 19:15:47 -0300

    feat: Create  ClienteRespository

---
+++ src/main/java/com/vsmgerenciadorclientes/repositories/ClienteRepository.java
@@ -0,0 +1,11 @@
+package com.vsmgerenciadorclientes.repositories;
+
+import com.vsmgerenciadorclientes.domain.cliente.Cliente;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+
+    public Cliente findByDocumento(String documento);
+}
