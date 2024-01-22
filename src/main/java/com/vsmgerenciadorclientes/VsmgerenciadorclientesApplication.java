99e1b485252fe0faf758f3f4315dfbd055ec8355
leandrosouzasnts <1eandrosouzasnts@gmail.com>
2024-01-22 19:02:38 -0300

    feat: Initial Config

---
+++ src/main/java/com/vsmgerenciadorclientes/VsmgerenciadorclientesApplication.java
@@ -0,0 +1,13 @@
+package com.vsmgerenciadorclientes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VsmgerenciadorclientesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VsmgerenciadorclientesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vsmgerenciadorclientes/VsmgerenciadorclientesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vsmgerenciadorclientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VsmgerenciadorclientesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
